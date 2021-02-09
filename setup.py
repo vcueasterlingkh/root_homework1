e0eb3a9b49345a4a7de78b425c6dc8d0eccb8e8f
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-02-09 18:49:13 -0500

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup file to describe my project."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="homemwork1",
+    version="0.1",
+    description="Homework 1 repository",
+    author="Chris Fauerbach",
+    author_email="chfauerbach@vcu.edu",
+    packages=find_packages(),
+)
