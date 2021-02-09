665a7c205b7d006cb5aa6d0565945922d85da7b5
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-02-09 18:46:32 -0500

    Updating to look better for linters

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -1,5 +1,6 @@
 """ This set of functions should return the appropriate data type"""
 
+
 def return_number_3():
     """ This function should return an integer with the value of 3"""
 
@@ -13,26 +14,25 @@ def return_string_vcu():
     return_value = None
     return return_value
 
+
 def return_lowercased_string(input_string):
-    """ You have a variable called input_string that is of type string.  
-        Return it but the lowercase version of it. """
+    """You have a variable called input_string that is of type string.
+    Return it but the lowercase version of it."""
 
     return_value = input_string
     return return_value
+
 
 def return_without_starting_ending_whitespace(input_string):
-    """ You have a variable called input_string that is of type string.
-        Return it but with the surrounding (left and right) whitespace stripped. """
+    """You have a variable called input_string that is of type string.
+    Return it but with the surrounding (left and right) whitespace stripped."""
 
     return_value = input_string
     return return_value
+
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
-    
+
     return_value = None
     return return_value
-
-
-   
-
--- tests/test_hw1.py
+++ tests/test_hw1.py
@@ -4,21 +4,42 @@
 from homework1 import hw1
 
 
-
 def test_return_number_3():
+    """Make sure the return of the function is 3, integer"""
+
     assert hw1.return_number_3() == 3
 
+
 def test_return_string_vcu():
+    """Make sure the return of the function is the string vcu"""
+
     assert hw1.return_string_vcu() == "vcu"
 
+
 def test_return_lowercased_string():
+    """Make sure the return of the function is the lowercased version of the parameter."""
+
     assert hw1.return_lowercased_string("HI THERE MOM") == "hi there mom"
+    assert hw1.return_lowercased_string("vcu.edu") == "vcu.edu"
+    assert (
+        hw1.return_lowercased_string("We Wish You a Merry Monday")
+        == "we wish you a merry monday"
+    )
 
 
-def test_return_without_starting_ending_whitespace(input_string):
-    assert hw1.return_without_starting_ending_whitespace("   asdfasdf    ") == "asdfasdf"
+def test_return_without_starting_ending_whitespace():
+    """Make sure the function strips the whitespace """
+
+    assert (
+        hw1.return_without_starting_ending_whitespace("   asdfasdf    ") == "asdfasdf"
+    )
+
 
 def test_return_addition():
+    """ Make sure the function returns the sum of the two parameters. """
+
     assert hw1.return_addition(1, 2) == 3
-
-
+    assert hw1.return_addition(100, 200) == 300
+    assert hw1.return_addition(43, 43) == 86
+    assert hw1.return_addition(50, 25) == 75
+    assert hw1.return_addition(19, -19) == 0
